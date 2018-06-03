dd74a6b4b0d938c5563e7480e055d0c082ccd5e1
tolavolaoke <tolavolaoke@hotmail.co.uk>
2018-06-03 23:15:14 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; // step 2 add to main component
+import { LNodeType } from '@angular/core/src/render3/interfaces/node';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  mainTitle = 'Tola Olaoke';
+  // variables
+  albumId: number;
+  id: number;
+  title: string = '';
+  url: string = '';
+  thumbnailUrl: string = '';
+  found: boolean;
+  photos: any[];
+  allPhotos: any[];
+  limit: number;
+  constructor(private httpClient: HttpClient) { }// Create a class
+
+
+  onNameKeyUp(event: any) {
+    this.id = event.target.value;
+    console.log(event.target.value);
+    this.found = true; // data will be false whilst typing
+  }
+
+  // Http Get Method
+  getPhotos() {
+    // this is where it is going to do the Http GET
+    this.httpClient.get(`https://jsonplaceholder.typicode.com/photos/?id=${this.id}`) // queries the URL by ID, the base address for api
+
+      .subscribe( // it subscribes to the URL, keeps watching it until it gets it and executes it
+        (data: any[]) => { // retrieves the object we call data from inside the URL
+          console.log(data);
+          if (data.length) { // if it returns some data
+            this.photos = data;
+          }
+        }
+      );
+  }
+
+  getAllPhotos() {
+    this.httpClient.get(`https://jsonplaceholder.typicode.com/photos`) // queries the URL by ID, the base address for api
+      .subscribe( // it subscribes to the URL, keeps watching it until it gets it and executes it
+        (data: any[]) => { // retrieves the object we call data from inside the URL
+          if (data.length) { // if it returns some data
+            this.allPhotos = data;
+            this.photos = this.allPhotos.filter((x, i) => i < 50);
+          }
+        }
+      );
+  }
+
+  changeLimit(limit) {
+    this.photos = this.allPhotos;
+    this.limit = limit;
+  }
+
+  ngOnInit() {
+    this.getAllPhotos();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http'; //imports the Http Client which prepares the setup for the API
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule //added in the NgModule declarations as a property
   ],
   providers: [],
   bootstrap: [AppComponent]
